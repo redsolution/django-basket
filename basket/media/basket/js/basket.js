5b784223f8c3d8d98f424348637830b817474b76
Ivan Gromov <summer.is.gone@gmail.com>
2010-04-21 02:01:10 +0600

    modified ajax javascript

--- basket/media/basket/js/basket.js
+++ basket/media/basket/js/basket.js
@@ -1,24 +1,23 @@
 ;
 $(document).ready(function() {
-	$('#refresh').click(function() {
+	$('#refresh-basket').click(function() {
 		$(this).after('<input type="hidden" name="ajax" value="1">');
 		return false;
 	});
-
-
 });
 
-function add_to_basket(item_id) {
+var basket={};
+basket.add_to_basket = function add_to_basket(item_id) {
 	$.post('/basket/add/', {'item': item_id}, function (){
-		reload_basket();
+		basket.reload_basket();
 	});
-}
+};
 
-function reload_basket() {
+basket.reload_basket = function reload_basket() {
 	var loading_div = $('.loading').clone();
 	$('#basket_panel p').prepend(loading_div);
 	loading_div.show();
 	$('#basket_panel').load('/basket/ajax/', null, function(){
 		loading_div.hide();
 	});
-}
+};
