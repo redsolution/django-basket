54ef4baaa499f53ebfe7075faaf255c4f1ef9384
Evgeniy Gulitsyn <evgeniy.gulitsyn@redsolution.ru>
2010-11-22 08:38:19 +0500

    Add order info model

--- basket/forms.py
+++ basket/forms.py
@@ -4,13 +4,25 @@ from django.conf import settings
 from django.forms.models import inlineformset_factory
 from django.template import loader
 from django.contrib.contenttypes.models import ContentType
-from basket.models import Order, BasketItem
+from basket.models import Order, BasketItem, get_order_info_class
 from basket.utils import import_item, send_mail
 from basket.settings import BASKET_FORM
 from django.utils import simplejson
+from django.core.exceptions import ImproperlyConfigured
+import datetime
 
 
-class BaseOrderForm(forms.ModelForm):
+#class BaseOrderForm(forms.ModelForm):
+#    '''
+#    Order basket form. If you want to override default fields,
+#    set BASKET_FORM in settings.py. Fields will be merged with 
+#    this base form. For more information check default settings
+#    in basket/settings.py
+#    '''
+#    class Meta:
+#        model = Order
+#        exclude = ('user', 'session', 'status', 'form_data')
+class OrderForm(forms.ModelForm):
     '''
     Order basket form. If you want to override default fields,
     set BASKET_FORM in settings.py. Fields will be merged with 
@@ -18,8 +30,23 @@ class BaseOrderForm(forms.ModelForm):
     in basket/settings.py
     '''
     class Meta:
-        model = Order
-        exclude = ('user', 'session', 'status', 'form_data')
+        model = get_order_info_class()
+        exclude = ('order', 'trans_company', 'delivery_type', 'delivery_cost',
+                   'delivery_datetime')
+
+    def save(self, **kwds):
+        orderinfo = super(OrderForm, self).save(**kwds)
+        orderinfo.registered = datetime.datetime.now()
+        orderinfo.save()
+        return orderinfo
+
+def get_order_form():
+    try:
+        form_class = import_item(BASKET_FORM, 'Can not import BASKET_FORM')
+    except ImproperlyConfigured:
+        return OrderForm
+    else:
+        return form_class
 
 
 class ContactForm(forms.ModelForm):
@@ -30,21 +57,21 @@ class ContactForm(forms.ModelForm):
     comment = forms.CharField(label=u'Комментарии', help_text='Поле не обязательное',
        max_length=200, required=False)
 
-extend_form_class = import_item(BASKET_FORM, 'Can not import BASKET_FORM')
-
-
-class OrderForm(extend_form_class, BaseOrderForm):
-
-    def save(self, *args, **kwds):
-        message = loader.render_to_string('basket/order.txt', {
-            'order': self.instance,
-            'data': self.cleaned_data,
-        })
-
-        self.instance.form_data = simplejson.dumps(self.cleaned_data)
-        send_mail(u'Форма заказа', message,
-            [manager[1] for manager in settings.MANAGERS])
-        return super(OrderForm, self).save(*args, **kwds)
+#extend_form_class = import_item(BASKET_FORM, 'Can not import BASKET_FORM')
+#
+#
+#class OrderForm(extend_form_class, BaseOrderForm):
+#
+#    def save(self, *args, **kwds):
+#        message = loader.render_to_string('basket/order.txt', {
+#            'order': self.instance,
+#            'data': self.cleaned_data,
+#        })
+#
+#        self.instance.form_data = simplejson.dumps(self.cleaned_data)
+#        send_mail(u'Форма заказа', message,
+#            [manager[1] for manager in settings.MANAGERS])
+#        return super(OrderForm, self).save(*args, **kwds)
 
 
 class OrderStatusForm(forms.Form):
