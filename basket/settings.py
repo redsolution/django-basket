b508cd4cea378039cddb66c47e72d58dde67202d
Alexander Ivanov <alexander.vl.ivanov@gmail.com>
2010-11-23 12:16:08 +0500

    Fix default form

--- basket/settings.py
+++ basket/settings.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 
-
-DEFAULT_BASKET_FORM = 'basket.forms.ContactForm'
+DEFAULT_BASKET_FORM = 'basket.forms.OrderForm'
 BASKET_FORM = getattr(settings, 'BASKET_FORM', DEFAULT_BASKET_FORM)
 PRICE_ATTR = getattr(settings, 'BASKET_PRICE_ATTR', 'price')
 DEFAULT_BASKET_ORDERINFO = 'basket.models.OrderInfo'
