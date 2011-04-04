0e46162f2d6f893c17489d743e3c7c9326a31e81
Ivan Gromov <ivan.gromov@redsolution.ru>
2011-04-04 16:29:52 +0600

    Removed old tags

--- basket/templatetags/basket_tags.py
+++ basket/templatetags/basket_tags.py
@@ -1,36 +1,7 @@
 from django import template
 from django.contrib.contenttypes.models import ContentType
-from basket.models import Order
-from basket.utils import uid_from_request
-from decimal import Decimal
 
 register = template.Library()
-
-@register.inclusion_tag('basket/history.html', takes_context=True)
-def show_basket_history(context):
-    """
-    Show history.
-    """
-    request = context.get('request', None)
-    if request:
-        history = Order.objects.history(uid_from_request(request))
-        last = Order.objects.get_last(uid_from_request(request))
-        if history:
-            history_sum = Decimal(sum([order.summary() for order in history]))
-        else:
-            history_sum = Decimal('0.00')
-    return locals()
-
-@register.inclusion_tag('basket/panel.html', takes_context=True)
-def show_basket_panel(context):
-    """
-    Show basket panel.
-    """
-    request = context.get('request', None)
-    if request:
-        return {'order': request.order}
-    else:
-        return {'order': None}
 
 @register.inclusion_tag('basket/button.html')
 def add_basket_button(object):
@@ -42,12 +13,3 @@ def add_basket_button(object):
     content_type = ContentType.objects.get_for_model(object)
     return {'content_type': content_type, 'object': object, }
 
-@register.simple_tag
-def content_type_id(object):
-    """
-    Render content type id.
-    """
-    if object is None:
-        return ''
-    content_type = ContentType.objects.get_for_model(object)
-    return '%d' % content_type.id
