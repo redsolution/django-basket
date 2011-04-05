31abc96281c03f98a90a15665225a6a583e3db43
Ivan Gromov <ivan.gromov@redsolution.ru>
2011-04-05 19:15:24 +0600

    Added decorators to views, fixed order_form instance creation

--- basket/views.py
+++ basket/views.py
@@ -6,9 +6,11 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
+from django.views.decorators.http import require_http_methods
 from django.http import HttpResponseBadRequest, HttpResponseRedirect, Http404
 from django.template import loader
 from basket.signals import order_submit
+
 
 @render_to('basket/basket.html')
 def basket(request):
@@ -31,7 +33,7 @@ def basket(request):
         formset = OrderFormset(instance=order)
 
     return {
-        'order_form': get_order_form(),
+        'order_form': get_order_form()(),
         'formset': formset,
         'order': order,
     }
@@ -63,46 +65,42 @@ def status(request, order_id=None):
     '''Use generic view instead'''
     raise NotImplementedError
 
+@require_http_methods(["POST"])
 @render_to('basket/summary.html')
 def add_to_basket(request):
-    if request.method == 'POST':
-        # Automatically create order if it does not exist
-        if request.order is None:
-            order = Order.from_request(request)
-        else:
-            order = request.order
-
-        content_type_id = request.REQUEST.get('content_type', None)
-        object_id = request.REQUEST.get('object_id', None)
-        try:
-            content_type = ContentType.objects.get(id=content_type_id)
-            item = content_type.get_object_for_this_type(id=object_id)
-        except ObjectDoesNotExist:
-            raise Http404
-
-        order.add_item(item)
-        return {'order': order}
+    # Automatically create order if it does not exist
+    if request.order is None:
+        order = Order.from_request(request)
     else:
-        return HttpResponseBadRequest(_('Only POST method allowed'))
+        order = request.order
+
+    content_type_id = request.REQUEST.get('content_type', None)
+    object_id = request.REQUEST.get('object_id', None)
+    try:
+        content_type = ContentType.objects.get(id=content_type_id)
+        item = content_type.get_object_for_this_type(id=object_id)
+    except ObjectDoesNotExist:
+        raise Http404
+
+    order.add_item(item)
+    return {'order': order}
 
 
+@require_http_methods(["POST"])
 @render_to('basket/summary.html')
 def delete_from_basket(request):
-    if request.method == 'POST':
-        # Do not automatically create order
-        order = request.order
-        if order is None:
-            raise Http404(_('Basket does not exist'))
+    # Do not automatically create order
+    order = request.order
+    if order is None:
+        raise Http404(_('Basket does not exist'))
 
-        content_type_id = request.REQUEST.get('content_type', None)
-        object_id = request.REQUEST.get('object_id', None)
-        try:
-            content_type = ContentType.objects.get(id=content_type_id)
-            item = content_type.get_object_for_this_type(id=object_id)
-        except ObjectDoesNotExist:
-            raise Http404
+    content_type_id = request.REQUEST.get('content_type', None)
+    object_id = request.REQUEST.get('object_id', None)
+    try:
+        content_type = ContentType.objects.get(id=content_type_id)
+        item = content_type.get_object_for_this_type(id=object_id)
+    except ObjectDoesNotExist:
+        raise Http404
 
-        order.remove_item(item)
-        return {'order': order}
-    else:
-        return HttpResponseBadRequest(_('Only POST method allowed'))
+    order.remove_item(item)
+    return {'order': order}
