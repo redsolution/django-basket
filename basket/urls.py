394fb4c96f272e56ca0822933a019658b21a9144
Ivan Gromov <ivan.gromov@redsolution.ru>
2011-04-06 13:03:32 +0600

    typo

--- basket/urls.py
+++ basket/urls.py
@@ -9,11 +9,11 @@ urlpatterns = patterns('basket.views',
 )
 
 if BASKET_OPTIONS_USE_DELETE:
-    urlpatterns += patterns('basket.veiws', 
+    urlpatterns += patterns('basket.views',
         url(r'^delete/$', 'delete_from_basket', name='del_from_basket'),
     )
 
-urlpatterns += patterns('django.views.generic.simple', 
+urlpatterns += patterns('django.views.generic.simple',
     url(r'^thankyou/$', 'direct_to_template', {'template': 'basket/thankyou.html'},
         name='basket-thankyou'),
     url(r'^empty/$', 'direct_to_template', {'template': 'basket/empty.html'},
