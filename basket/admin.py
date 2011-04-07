1c62b9f53e2f64802830efa159230935be0bab49
Ivan Gromov <summer.is.gone@gmail.com>
2011-04-08 02:14:15 +0600

    Changed admin widget for status comment

--- basket/admin.py
+++ basket/admin.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from django import forms
+from django import forms
 from basket.models import Order, Status
 from django.utils.translation import ugettext_lazy as _
 
+class StatusAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Status
+
+    comment = forms.CharField(
+        widget=admin.widgets.AdminTextareaWidget(attrs={'rows': 5}))
 
 class StatusInline(admin.TabularInline):
     model = Status
+    form = StatusAdminForm
     extra = 0
     fields = ('status', 'order', 'modified', 'comment',)
 
