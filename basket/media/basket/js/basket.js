335394e3b5051baa221dbe2226dce77750a16251
Ivan Gromov <ivan.gromov@redsolution.ru>
2011-04-11 18:25:13 +0600

    JSLint'ed script

--- basket/media/basket/js/basket.js
+++ basket/media/basket/js/basket.js
@@ -27,17 +27,18 @@ basket.add = function(el, content_type_id, object_id){
 		}}
 	);
 	return false;
-}
+};
 
 $('html').ajaxSend(function(event, xhr, settings) {
     function getCookie(name) {
         var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
+        if (document.cookie && document.cookie !== '') {
             var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
+            var i;
+            for (i = 0; i < cookies.length; i++) {
                 var cookie = jQuery.trim(cookies[i]);
                 // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
                     cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                     break;
                 }
