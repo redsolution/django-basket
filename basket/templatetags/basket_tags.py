7a18dbce454608f71ff84db6106abec3b42750d5
Victor Smirnov <victor.smirnov@redsolution.ru>
2011-09-14 16:19:49 +0600

    added arguments to add_basket_button tag

--- basket/templatetags/basket_tags.py
+++ basket/templatetags/basket_tags.py
@@ -1,17 +1,59 @@
+from classytags.core import Tag, Options
+from classytags.arguments import Argument
+from classytags.helpers import InclusionTag
 from django import template
 from django.contrib.contenttypes.models import ContentType
+from basket.models import *
 
 register = template.Library()
 
-@register.inclusion_tag('basket/button.html')
-def add_basket_button(object):
-    """
-    Add button to add specified object to the basket.
-    """
-    if object is None:
-        return {'content_type': None, 'object': None, }
-    content_type = ContentType.objects.get_for_model(object)
-    return {'content_type': content_type, 'object': object, }
+class AddBasketButton(InclusionTag):
+    name = 'add_basket_button'
+    template = 'basket/button.html'
+    
+    options = Options(
+        Argument('object', required=True, resolve=True),
+        Argument('button_text', required=True, resolve=False),
+        Argument('added_text', required=True, resolve=False),
+        Argument('already_in_text', required=True, resolve=False),
+        Argument('template_name', required=False, resolve=False),
+    )
+
+    def get_template(self, context, object, button_text, added_text, already_in_text, template_name):
+        if template_name:
+            template_to_render = template_name
+        else:
+            template_to_render = self.template
+        return template_to_render
+    
+    def get_context(self, context, object, button_text, added_text, already_in_text, template_name):
+        context_to_return = {}
+        if object is None:
+            context_to_return.update({
+                'content_type': None,
+                'object': None,
+            })
+        else:
+            content_type = ContentType.objects.get_for_model(object)
+            order_id = context['request'].session['order_id']
+            
+            # quantity of such items those are already into basket
+            already_in_qty = BasketItem.objects.filter(order=order_id, content_type=content_type, object_id=object.id).count()
+            
+            # is any number of such items in the basket
+            already_in = bool(already_in_qty)
+            
+            context_to_return.update({
+                'content_type': content_type,
+                'object': object,
+                'button_text': button_text,
+                'added_text': added_text,
+                'already_in': already_in,
+                'already_in_text': already_in_text,
+            })
+        return context_to_return
+
+register.tag(AddBasketButton)
 
 @register.filter
 def content_type(object):
